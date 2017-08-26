daaedf130abe7be4b9eb79841bf880b6f3c4ed37
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-26 14:46:06 -0600

    Add getAuth route to router.

--- src/server/router.js
+++ src/server/router.js
@@ -2,11 +2,14 @@ const express = require('express');
 
 const router = express.Router();
 const locationController = require('./locationController');
+const authController = require('./authController');
 
 router.get('/v1/locations', locationController.getLocations);
 router.post('/v1/locations', locationController.postLocation);
 router.get('/v1/locations/:id', locationController.getLocationById);
 router.put('/v1/locations/:id', locationController.putLocation);
 router.delete('/v1/locations/:id', locationController.deleteLocation);
+
+router.post('/v1/auth', authController.getAuth);
 
 module.exports = router;
