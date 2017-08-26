1ded1d5fcd71d6d0d5587d84d598f0cc4eed4d7e
Jason Collins <the.oem@gmail.com>
2017-08-26 10:53:24 -0600

    Add updated rules for linter.

--- test/integration/cameras.routes.spec.js
+++ test/integration/cameras.routes.spec.js
@@ -93,4 +93,44 @@ describe('Testing Cameras API Routes', () => {
         });
     });
   });
+
+  describe('POST /api/v1/cameras', () => {
+    it('should respond with a success message and the newly added camera', (done) => {
+      chai.request(server)
+        .post('/api/v1/camera')
+        .send({
+          model: 'My new sweet camera',
+          max_resolution: 1080,
+          low_resolution: 1080,
+          effective_pixels: 0,
+          zoom_wide: 38,
+          zoom_tele: 114,
+          normal_focus_range: 70,
+          macro_focus_range: 40,
+          storage_included: 4,
+          weight: 420,
+          dimensions: 95,
+          price: 179,
+        })
+        .end((err, res) => {
+          // there should be no errors
+          should.not.exist(err);
+          // there should be a 201 status code
+          // (indicating that something was "created")
+          res.status.should.equal(201);
+          // the response should be JSON
+          res.type.should.equal('application/json');
+          // the JSON response body should have a
+          // key-value pair of {"status": "success"}
+          res.body.status.should.eql('success');
+          // the JSON response body should have a
+          // key-value pair of {"data": 1 user object}
+          res.body.data[0].should.include.keys(
+            'dimensions', 'effective_pixels', 'id', 'low_resolution',
+            'macro_focus_range', 'max_resolution', 'model', 'normal_focus_range', 'price',
+            'storage_included', 'weight', 'zoom_tele', 'zoom_wide');
+          done();
+        });
+    });
+  });
 });
