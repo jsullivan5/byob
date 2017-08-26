4a3917c6f5744c852c668c62d5c6257fb2b8c1e8
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-26 12:59:04 -0600

    Add getAuth function to authController.

--- src/server/authController.js
+++ src/server/authController.js
@@ -1,3 +1,26 @@
-const authJWT = (req, res) => true;
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
-module.exports = { authJWT };
+const getAuth = (req, res) => {
+  const email = req.body.email.toLowerCase();
+  const appName = req.body.appName;
+  const secret = process.env.SECRET_KEY;
+  const admin = email.endsWith('turing.io') ? { admin: true } : null;
+
+  const initPayload = {
+    email,
+    appName,
+  };
+
+  const finalPayload = Object.assign(initPayload, admin);
+  const token = jwt.sign(finalPayload, secret, { expiresIn: '48h' });
+
+  res.status(201).json({ token });
+};
+
+const checkAuth = (req, res) => true;
+
+module.exports = {
+  getAuth,
+  checkAuth,
+};
