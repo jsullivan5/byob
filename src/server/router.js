c5efb5e723e55532b1c9eea60fc739abd0c3f43b
Jason Collins <the.oem@gmail.com>
2017-08-26 09:55:31 -0600

    Fix merge conflicts.

--- src/server/router.js
+++ src/server/router.js
@@ -2,7 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 const cameraController = require('./cameraController');
+const locationController = require('./locationController');
 
 router.get('/v1/cameras', cameraController.getCameras);
+router.get('/v1/cameras/:id', cameraController.getCamerasById);
+router.get('/v1/locations', locationController.getLocations);
 
 module.exports = router;
