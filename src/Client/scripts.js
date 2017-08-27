bb24cf5ec4d242d6b3bf90d5baf0fdcaf65ab9c3
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-27 15:39:50 -0600

    Add fetch token to client side.

--- src/Client/scripts.js
+++ src/Client/scripts.js
@@ -1,7 +1,28 @@
 /* global document */
+/* global fetch */
 
 const submit = document.getElementById('form-submit');
 
 submit.addEventListener('click', (event) => {
   event.preventDefault();
+  const displayMsg = document.getElementById('jwt-message');
+  const email = document.getElementById('form-email').value;
+  const appName = document.getElementById('form-app-name').value;
+
+  console.log(email, appName);
+
+  fetch('/api/v1/auth', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      email,
+      appName,
+    })
+  })
+    .then(response => response.json())
+    .then((data) => {
+      console.log(data);
+      displayMsg.innerText = JSON.stringify(data);
+    })
+    .catch(error => console.log(error));
 });
--- src/server/server.js
+++ src/server/server.js
@@ -11,6 +11,7 @@ app.use(express.static(path.join(__dirname, '../Client')));
 
 app.set('port', process.env.PORT || 3000);
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use('/api', router);
