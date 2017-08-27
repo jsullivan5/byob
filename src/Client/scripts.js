04e0a4f693bbeb53e0380da78e4ecb7850f58a6a
James Sullivan <jamesfsullivan5@gmail.com>
2017-08-27 17:34:46 -0600

    Add form validation to UI.

--- src/Client/scripts.js
+++ src/Client/scripts.js
@@ -1,12 +1,14 @@
 /* global document */
 /* global fetch */
-
+/* global alert */
 
 const submit = document.getElementById('form-submit');
 const emailInput = document.getElementById('form-email');
 const appNameInput = document.getElementById('form-app-name');
 
 submit.addEventListener('click', handleSubmit);
+emailInput.addEventListener('change', disableBtn);
+appNameInput.addEventListener('change', disableBtn);
 
 function handleSubmit(event) {
   event.preventDefault();
@@ -14,7 +16,10 @@ function handleSubmit(event) {
   const email = document.getElementById('form-email').value;
   const appName = document.getElementById('form-app-name').value;
 
-  console.log(email, appName);
+  if (!validateEmail(email)) {
+    alert('Please include a valid email address.');
+    return;
+  }
 
   fetch('/api/v1/auth', {
     method: 'POST',
@@ -30,4 +35,17 @@ function handleSubmit(event) {
       displayMsg.innerText = JSON.stringify(data);
     })
     .catch(error => console.log(error));
+}
+
+function disableBtn() {
+  if (emailInput !== '' && appNameInput !== '') {
+    submit.disabled = false;
+  } else {
+    submit.disabled = true;
+  }
+}
+
+function validateEmail(email) {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(email);
 }
